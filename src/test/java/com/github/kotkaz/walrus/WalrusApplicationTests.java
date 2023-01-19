cf219415a9789b6d374ca9331a2b9e57904dfa25
Karl Rapur <rapurkarl@gmail.com>
2023-01-19 20:59:10 +0200

    Add context load test.

--- src/test/java/com/github/kotkaz/walrus/WalrusApplicationTests.java
+++ src/test/java/com/github/kotkaz/walrus/WalrusApplicationTests.java
@@ -1,13 +1,31 @@
 package com.github.kotkaz.walrus;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.github.kotkaz.walrus.api.CityExplorerApi;
+import com.github.kotkaz.walrus.mapper.CityMapper;
+import com.github.kotkaz.walrus.repository.CityRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class WalrusApplicationTests {
 
+    @Autowired
+    private CityExplorerApi cityExplorerApi;
+
+    @Autowired
+    private CityRepository cityRepository;
+
+    @Autowired
+    private CityMapper cityMapper;
+
     @Test
     void contextLoads() {
+        assertNotNull(cityExplorerApi);
+        assertNotNull(cityRepository);
+        assertNotNull(cityMapper);
     }
 
 }
